ffbc18c57660f396e52f8ef46afca25605edbd59
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-03-08 18:34:55 +0100

    finished some, few remains to be solved

--- src/05_fetch.js
+++ src/05_fetch.js
@@ -9,15 +9,28 @@ const NicosRepositories = `https://api.github.com/users/minimumviableperson/repo
 //  - fetch data from the NicosRepositories endpoint.
 //  - only keep the .name property of each repository.
 //  - return all Nico's repo names as an array of strings.
+
 let array = []
 
-function getNamesOfNicosRepos() {
-    fetch("https://localhost:4000/NicosRepositories")
-        .then(resp => resp.json())
-        .then(arrayOfStringsFromServer => setArray(arrayOfStringsFromServer))
+async function getNamesOfNicosRepos() {
+
+    const arrayFetching = await fetch(NicosRepositories)
+    return arrayFetching
+
 }
 
+const fullArray = getNamesOfNicosRepos()
+
+//@ts-ignore
+for (const element of fullArray) {
+    array.push(element.name)
+}
+
+console.log(fullArray)
+console.log(array)
+
 // 2. Create a JSON server. Add a /posts endpoint to it, which returns a list of posts.
+
 // A post is: {
 //   id: number,
 //   title: string,
@@ -47,8 +60,30 @@ function getNamesOfNicosRepos() {
 // - save your post in the JSON server
 // - console.log the newly created post from the response (it must contain the new post's id);
 
-function getNamesOfNicosRepos() {
-    fetch("https://localhost:4000/NicosRepositories")
-        .then(resp => resp.json())
-        .then(arrayOfStringsFromServer => setArray(arrayOfStringsFromServer))
+function createPost(post) {
+
+    fetch(NicosRepositories, {
+        
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify(post)
+
+    })
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err=>console.log(err))
+
 }
+
+const post = {
+    id: 55,
+    name: "jurgen",
+    surname: "dont know",
+    age:45
+}
+
+createPost(post)
